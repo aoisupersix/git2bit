413938ff11c1e1f44a1d2321550708266634fb3d
a_tanaka <a_tanaka@timedia.co.jp>
2019-10-31 09:35:27 +0000

    [add] argument parsing

--- transfer.py
+++ transfer.py
@@ -9,3 +9,6 @@ parser.add_argument('-be', '--bitbucket_endpoint', default='https://api.bitbucke
 parser.add_argument('-bo', '--bitbucket_owner', required=True)
 parser.add_argument('-br', '--bitbucket_repo', required=True)
 parser.add_argument('-bt', '--bitbucket_token', required=True)
+
+args = parser.parse_args()
+print(args)
