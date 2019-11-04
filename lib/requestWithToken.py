894a191986d9894663f031e0f7472cbe21ab6592
aoisupersix <aoisupersix@gmail.com>
2019-11-04 00:41:29 +0000

    [fix] getRequestWithToken not returned response

--- lib/requestWithToken.py
+++ lib/requestWithToken.py
@@ -6,4 +6,4 @@ def getRequestWithToken(url, token):
     引数に与えられたURLに認証トークン付きでGetリクエストを送信し、結果を返します。
     """
     header = {'Authorization': f'token {token}'}
-    requests.get(url, headers=header)
+    return requests.get(url, headers=header)
