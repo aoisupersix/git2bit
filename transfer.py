da6dde6df01dcdbf435b080ea6b0b66f58cbb8ab
a_tanaka <a_tanaka@timedia.co.jp>
2019-11-05 08:17:46 +0000

    [add] download issues saving process

--- transfer.py
+++ transfer.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from lib.models import argumentParser as parser
 from lib.models import gitbucketApi
 
@@ -10,3 +13,9 @@ gitbucket = gitbucketApi.GitbucketApi(
 )
 
 issues = sorted(gitbucket.getAllIssues(), key=lambda x: x['number'])  # チケット番号でソート
+
+# 特に使い道はないけど後で確認したいとき用に取得したデータを保存しておく
+os.makedirs('./data', exist_ok=True)
+f = open(f'./data/gitbucket_issues_{args.gitbucket_owner}-{args.gitbucket_repo}.json', 'w')
+json.dump(issues, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
+f.close()
