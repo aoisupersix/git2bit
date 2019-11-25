9f4242c03d4ee1f6f94bb48a86f5f5105c5af42c
a_tanaka <a_tanaka@timedia.co.jp>
2019-11-25 15:14:58 +0900

    [add] gitbucketapi.getIssuesPerPage test

--- tests/models/test_gitbucket_api.py
+++ tests/models/test_gitbucket_api.py
@@ -1,7 +1,27 @@
 from lib.models.gitbucket_api import GitbucketApi
 
 
-def test_2():
-    # TODO
-    api = GitbucketApi('', '', '', '')
-    assert api.baseUrl == ''
+def test_getIssuesPerPage_RequestParameters(mocker):
+    requests = mocker.MagicMock(**{
+        'return_value.ok': True,
+        'return_value.json.return_value': {}
+    })
+
+    mocker.patch('requests.get', requests)
+    GitbucketApi('endpoint', 'owner', 'repo', 'token').getIssuesPerPage(1)
+    issues_endpoint, optional_args = requests.call_args
+
+    # Check Request Header
+    assert 'headers' in optional_args
+    header = optional_args['headers']
+    assert header['Authorization'] == 'token token'
+    assert header['content-type'] == 'application/json'
+
+    assert 'params' in optional_args
+    payload = optional_args['params']
+    assert payload['page'] == 1
+    assert payload['state'] == 'open'
+
+    assert issues_endpoint[0] == 'endpoint/api/v3/repos/owner/repo/issues'
+    assert header['Authorization'] == 'token token'
+    assert header['content-type'] == 'application/json'
