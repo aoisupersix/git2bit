83f95ffc5dae7214d3a7796540058b75c58fc86d
aoisupersix <aoisupersix@gmail.com>
2019-12-22 03:05:29 +0000

    [add] load mapping from file path method

--- lib/models/id_converter.py
+++ lib/models/id_converter.py
@@ -1,3 +1,6 @@
+import yaml
+
+
 class IdConverter:
     """
     GitbucketのユーザIDとBitbucketのユーザIDを変換します
@@ -5,6 +8,14 @@ class IdConverter:
 
     def __init__(self, mapping: dict):
         self.__mapping = mapping
+
+    def loadMappingFromFilePath(self, mappingFilePath: str):
+        """
+        引数に指定されたファイルからマッピングを読み込みます
+        """
+
+        with open(mappingFilePath, 'r') as mappingFile:
+            self.__mapping = yaml.load(mappingFile)
 
     def convertToBitbucketId(self, gitbucketId: str) -> str:
         """
