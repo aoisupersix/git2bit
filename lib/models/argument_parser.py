8653bba4c38a12ac40a0e1dca04877b1a237d313
aoisupersix <aoisupersix@gmail.com>
2020-10-24 22:14:14 +0900

    [add] argument help text

--- lib/models/argument_parser.py
+++ lib/models/argument_parser.py
@@ -2,12 +2,18 @@ import argparse
 
 
 def parse() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='transfer your gitbucket issues to bitbucket.')
-    parser.add_argument('-ge', '--gitbucket_endpoint', required=True)
-    parser.add_argument('-go', '--gitbucket_owner', required=True)
-    parser.add_argument('-gr', '--gitbucket_repo', required=True)
-    parser.add_argument('-gt', '--gitbucket_token', required=True)
-    parser.add_argument('-m', '--mapping', required=False)
+    parser = argparse.ArgumentParser(
+        description='transfer your Gitbucket issues to Bitbucket.')
+    parser.add_argument('-ge', '--gitbucket_endpoint', required=True,
+                        help='Gitbucket API endpoint URL. (http(s)://yourgitbucket/api/v3)')
+    parser.add_argument('-go', '--gitbucket_owner', required=True,
+                        help='Gitbucket owner user or group name from which the issue was migrated.')
+    parser.add_argument('-gr', '--gitbucket_repo', required=True,
+                        help='Gitbucket repository name from which the issue was migrated.')
+    parser.add_argument('-gt', '--gitbucket_token', required=True,
+                        help='Personal access token to access the Gitbucket API.')
+    parser.add_argument('-m', '--mapping', required=False,
+                        help='Path from Gitbucket to Bitbucket user id mapping definition file. (*.yml)')
 
     args = parser.parse_args()
     args.gitbucket_endpoint = removeTrailingSlash(args.gitbucket_endpoint)
