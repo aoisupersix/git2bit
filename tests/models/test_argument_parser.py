c7eea8b7154c299aff0455c9dc2c9edc0d915375
aoisupersix <aoisupersix@gmail.com>
2020-10-24 23:31:33 +0900

    [update] arguement parser test

--- tests/models/test_argument_parser.py
+++ tests/models/test_argument_parser.py
@@ -21,25 +21,31 @@ def test_parse_RequiredArguments(mocker: MockFixture, argv: list):
     mocker.patch('sys.argv', argv)
     args = parse()
 
-    assert len(vars(args)) == 5
+    assert len(vars(args)) == 7
     assert args.api_endpoint == 'endpoint'
     assert args.owner == 'owner'
     assert args.repo == 'repo'
     assert args.token == 'token'
     assert args.mapping is None
+    assert args.output is None
+    assert args.write_apiresponse is False
 
 
 def test_parse_OptionalArguments(mocker: MockFixture, argv: list):
     argv.append('--mapping=mapping')
+    argv.append('--output=output')
+    argv.append('--write-apiresponse')
     mocker.patch('sys.argv', argv)
     args = parse()
 
-    assert len(vars(args)) == 5
+    assert len(vars(args)) == 7
     assert args.api_endpoint == 'endpoint'
     assert args.owner == 'owner'
     assert args.repo == 'repo'
     assert args.token == 'token'
     assert args.mapping == 'mapping'
+    assert args.output == 'output'
+    assert args.write_apiresponse
 
 
 def test_removeTrailingSlash_Removing():
